651cd43123060653578092386ec03a52ddf64475
Adam Schulte <adamschulte148@hotmail.com>
2022-01-14 23:17:27 +0000

    adding index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  
+  for (let num of array) {
+    // console.log(Array.isArray(num))
+    if (Array.isArray(num)) {
+        sum += sumItems(num) 
+    } else {
+      sum += num      
+    }
+  }
+  console.log(sum)
+  return sum
 }
+
+sumItems([[1, 2, [[3], 4]], 5, []])
+
+// console.log(sumItems([1,2,[1,2]]))
+
 
 module.exports = sumItems;
